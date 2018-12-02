5f9c12a36aff512856557db6415dd896df079517
Kenn <kennbro@protonmail.com>
2018-12-02 18:36:39 -0300

    [FIX] Comment lines in Controller

--- frontend/src/app/pages/gatherer/controllerWidgets/githubCalendar.js
+++ frontend/src/app/pages/gatherer/controllerWidgets/githubCalendar.js
@@ -7,12 +7,12 @@
   function githubCalendar($scope, $rootScope, $http) {
     console.log('Initialize Github Calendar Controller');
 
-    $scope.$watch('gather.github.result[3].graphic[1].cal_actual', function(texts) {
-        $scope.gather.github.result[3].graphic[1].cal_actual = texts.replace(/ebedf0/g, "666666");
-    });
-    $scope.$watch('gather.github.result[3].graphic[2].cal_previous', function(texts) {
-        $scope.gather.github.result[3].graphic[2].cal_previous = texts.replace(/ebedf0/g, "666666");
-    });
+    // $scope.$watch('gather.github.result[4].graphic[1].cal_actual', function(texts) {
+    //     $scope.gather.github.result[3].graphic[1].cal_actual = texts.replace(/ebedf0/g, "666666");
+    // });
+    // $scope.$watch('gather.github.result[4].graphic[2].cal_previous', function(texts) {
+    //     $scope.gather.github.result[3].graphic[2].cal_previous = texts.replace(/ebedf0/g, "666666");
+    // });
   }
 
 })();
