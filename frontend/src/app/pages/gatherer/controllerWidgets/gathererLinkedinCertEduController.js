67fc36b9789f1f7eefe29479fd30f7d037d97dee
Kenn <kennbro@protonmail.com>
2018-12-02 23:13:51 -0300

    [ADD] Frontend graphs and image

---
+++ frontend/src/app/pages/gatherer/controllerWidgets/gathererLinkedinCertEduController.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.gatherer')
+    .controller('gathererLinkedinCertEduController', gathererLinkedinCertEduController);
+
+  function gathererLinkedinCertEduController($scope, $rootScope, $http) {
+    console.log('Initialize LinkedinCertEdu Controller');
+  }
+
+})();
