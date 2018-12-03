ce254de6fe9ccad0e1fd042cad6755e5687dc364
Kenn <kennbro@protonmail.com>
2018-12-02 23:05:40 -0300

    [ADD] Configuration Celery and API for linkedin

--- backend/factories/celery_config.py
+++ backend/factories/celery_config.py
@@ -7,6 +7,7 @@ app = Celery('celery_config', broker='redis://localhost:6379/0',
                       'modules.leaks.leaks_tasks',
                       'modules.keybase.keybase_tasks',
                       'modules.twitter.twitter_tasks',
+                      'modules.linkedin.linkedin_tasks',
                       'modules.username.username_tasks',
                       'modules.fullcontact.fullcontact_tasks',
                       ])
