2c59046a42fd599a74577bca3be4671e5c9e7eb2
Kenn <kennbro@protonmail.com>
2019-07-04 08:35:01 -0300

    [MOD] Font replace

--- frontend/src/app/@theme/components/footer/footer.component.ts
+++ frontend/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">I Know You - 2019</span>
+    <span class="created-by" style="color: #05fcfc; font-family: Exo; font-size: 1.125rem; font-weight: 400;">I Know You - 2019</span>
     <div class="socials">
       <a href="https://www.twitter.com/kennbroorg" target="_blank" class="ion ion-social-twitter"></a>
     </div>
