4631005bc470e0c4ab51f6588647d3114b59c926
Kenn <kennbro@protonmail.com>
2019-07-04 10:49:31 -0300

    [MOD] Change presentation and principal page

--- frontend/src/app/pages/principal/principal.module.ts
+++ frontend/src/app/pages/principal/principal.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 import { ThemeModule } from '../../@theme/theme.module';
 import { PrincipalComponent } from './principal.component';
 import { LogoIkyComponent } from './logo-iky/logo-iky.component';
@@ -7,6 +9,7 @@ import { LogoIkyComponent } from './logo-iky/logo-iky.component';
 @NgModule({
   imports: [
     ThemeModule,
+    NgxChartsModule, 
   ],
   declarations: [
     PrincipalComponent,
