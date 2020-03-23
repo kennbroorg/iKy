f10fc10b9c0eb040a3aa7a37fa9a973305232c46
Kenn <kennbro@protonmail.com>
2020-03-23 14:49:21 -0300

    [IMP] Improve Vagranfile

--- install/vagrant/checks.sh
+++ install/vagrant/checks.sh
@@ -4,14 +4,42 @@ RED='\033[0;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-CHECK=$(curl --head --silent -S localhost:4200 2>&1)
+echo ""
+echo "Running checks..."
+echo "Checking NODE..."
+
+CHECK_NPM=$(curl --head --silent -S localhost:4200 2>&1)
 STATUS=$?
-MSG=$(echo "${GULP_CHECK}" | head -n 1)
+MSG=$(echo "${CHECK_NPM}" | head -n 1)
 
 if [ $STATUS -eq 0 ]; then
-    echo -e "${GREEN}INFO:${NC} $MSG"
+    echo -e "${GREEN}INFO NPM:${NC} $MSG"
 else
-    echo -e "${RED}ERROR:${NC} $MSG"
+    echo -e "${RED}ERROR NPM:${NC} $MSG"
+fi
+
+echo ""
+echo "Checking CELERY..."
+CHECK_CELERY=$(curl --head --silent -S --header "Content-Type: application/json" --request POST --data '{"username": "x", "from": ""}' http://127.0.0.1:5000/github 2>&1)
+STATUS=$?
+MSG=$(echo "${CHECK_CELERY}" | head -n 1)
+
+if [ $STATUS -eq 0 ]; then
+    echo -e "${GREEN}INFO CELERY:${NC} $MSG"
+else
+    echo -e "${RED}ERROR CELERY:${NC} $MSG"
+fi
+
+echo ""
+echo "Checking APP..."
+CHECK_APP=$(curl --head --silent -S localhost:5000/tasklist 2>&1)
+STATUS=$?
+MSG=$(echo "${CHECK_APP}" | head -n 1)
+
+if [ $STATUS -eq 0 ]; then
+    echo -e "${GREEN}INFO APP:${NC} $MSG"
+else
+    echo -e "${RED}ERROR APP:${NC} $MSG"
 fi
 
 exit $STATUS
