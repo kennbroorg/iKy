82fdf2d49c0c718b5021f4a44eb51e7cd291c196
KennBro <kennbro@protonmail.com>
2020-06-15 22:28:07 -0300

    [ADD] Adapt to Twint

--- backend/modules/tweetiment/tweetiment_tasks.py
+++ backend/modules/tweetiment/tweetiment_tasks.py
@@ -4,11 +4,8 @@
 import sys
 import json
 import requests
-# import urllib3
 import re
 
-import tweepy
-# import oauth2
 import redis
 from langdetect import detect
 from googletrans import Translator
@@ -19,7 +16,6 @@ from datetime import datetime
 try:
     from factories._celery import create_celery
     from factories.application import create_application
-    from factories.configuration import api_keys_search
     from celery.utils.log import get_task_logger
     celery = create_celery(create_application())
 except ImportError:
@@ -27,7 +23,6 @@ except ImportError:
     sys.path.append('../../')
     from factories._celery import create_celery
     from factories.application import create_application
-    from factories.configuration import api_keys_search
     from celery.utils.log import get_task_logger
     celery = create_celery(create_application())
 
@@ -35,13 +30,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 logger = get_task_logger(__name__)
-
-# Compatibility code
-try:
-    # Python 2: "unicode" is built-in
-    unicode
-except NameError:
-    unicode = str
 
 
 @celery.task
@@ -52,15 +40,21 @@ def t_tweetiment(username, task_id, from_m="Initial"):
     pos = []
     neu = []
     compound = []
+    tweets = []
     task_id_complete = "celery-task-meta-" + task_id
 
     print("TaskID : " + task_id_complete)
 
     redis_db = redis.Redis(host='localhost', port=6379, db=0)
     value = redis_db.get(task_id_complete)
+
+    # import pdb; pdb.set_trace()
     try:
         json_value = json.loads(value)
-        tweets = json_value['result'][3]['raw'][1]['raw_node_tweets']
+        tweets_json = json_value['result'][3]['raw'][1]['raw_node_tweets']
+        tweets_json = json.loads(tweets_json)['tweet']
+        for tweet in tweets_json:
+            tweets.append(tweets_json[tweet])
     except:
         tweets = []
 
@@ -69,34 +63,34 @@ def t_tweetiment(username, task_id, from_m="Initial"):
 
     c = 0
     for tweet in tweets:
-        if (tweet['text'][:3] != 'RT '):
-            c = c + 1
-            # Get date
-            create_date = tweet['created_at']
-            date_format = datetime.strptime(create_date, "%a %b %d %H:%M:%S +0000 %Y")
-            create_date = date_format.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-            # Remove mentions
-            text = re.sub(r'@\w+\s+', "", tweet['text'])
-            # Remove URLs
-            text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-            # Remove emojis
-            RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
-            text = RE_EMOJI.sub(r'', text)
-            if (len(text) > 3):
-                translated = text
-                lang_detect = detect(text)
-                if (lang_detect != 'en'):
-                    trans = translator.translate(text)
-                    translated = trans.text
-                    # sentences.append(trans.text)
-                vs = analyzer.polarity_scores(translated)
+        create_date = c
+        c = c + 1
+        # Get date
+        # create_date = tweet['created_at']
+        # date_format = datetime.strptime(create_date, "%a %b %d %H:%M:%S +0000 %Y")
+        # create_date = date_format.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        # Remove mentions
+        text = re.sub(r'@\w+\s+', "", tweet)
+        # Remove URLs
+        text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+        # Remove emojis
+        RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+        text = RE_EMOJI.sub(r'', text)
+        if (len(text) > 3):
+            translated = text
+            lang_detect = detect(text)
+            if (lang_detect != 'en'):
+                trans = translator.translate(text)
+                translated = trans.text
+                # sentences.append(trans.text)
+            vs = analyzer.polarity_scores(translated)
 
-                raw.append({"text": translated, "date": create_date,
-                            "sentiment": vs})
-                neg.append({"name": create_date, "value": vs["neg"]})
-                pos.append({"name": create_date, "value": vs["pos"]})
-                neu.append({"name": create_date, "value": vs["neu"]})
-                compound.append({"name": create_date, "value": vs["compound"]})
+            raw.append({"text": translated, "date": create_date,
+                        "sentiment": vs})
+            neg.append({"name": create_date, "value": vs["neg"]})
+            pos.append({"name": create_date, "value": vs["pos"]})
+            neu.append({"name": create_date, "value": vs["neu"]})
+            compound.append({"name": create_date, "value": vs["compound"]})
 
     # Total
     total = []
