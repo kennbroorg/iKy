40d75cf0279335dcf1eefbc347bec5e19c6d1387
KennBro <kennbro@protonmail.com>
2020-06-18 01:08:18 -0300

    [ADD] MediaType frontend to instagram component

---
+++ frontend/src/app/pages/gatherer/instagram/instagram-mediatype/instagram-mediatype.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, TemplateRef } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+
+@Component({
+    selector: 'ngx-instagram-mediatype',
+    templateUrl: './instagram-mediatype.component.html',
+    styleUrls: ['./instagram-mediatype.component.scss']
+})
+export class InstagramMediatypeComponent implements OnInit, AfterViewInit {
+    @ViewChild('nbCardInstagramMediatype', { static: true }) private cardContainer: ElementRef;
+    @Input() private data: any;
+    private instagramMediatype : any;
+
+    private card: any;
+    private width: number;
+    private height: number;
+    showLegend = true;
+    showLabels = true;
+
+    colorScheme = {
+      domain: [ 
+          '#80deea', 
+          '#4dd0e1',
+          '#26c6da', 
+          '#00bcd4', 
+          '#00acc1', 
+          '#0097a7', 
+          '#00838f', 
+          '#006064'
+      ]
+    };
+
+    constructor(private dialogService: NbDialogService) {}
+
+    ngOnInit() {
+        this.card = this.cardContainer.nativeElement;
+    }
+
+    ngAfterViewInit() {
+        this.width = this.cardContainer.nativeElement.parentNode.parentNode.clientWidth;
+        this.height = this.cardContainer.nativeElement.parentNode.parentNode.clientHeight - 55;
+        console.log("Instagram Mediatype Component");
+
+        this.instagramMediatype = this.data.result[4].graphic[8].mediatype;
+    }
+
+    openDialog(dialog: TemplateRef<any>) {
+        this.dialogService.open(dialog);
+    }
+}
