fa97f9754e939dc4aa19512ee480dae61ef589be
KennBro <kennbro@protonmail.com>
2023-11-17 19:42:42 -0300

    [MOD] fontawesome search and list

--- backend/factories/fontcheat.py
+++ backend/factories/fontcheat.py
@@ -5,46 +5,21 @@ import requests
 import re
 import os
 import json
+import fontawesome as fa
 
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+# from requests.packages.urllib3.exceptions import InsecureRequestWarning
+# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def fontawesome_cheat_5():
-    """ Get icon name for fontawesome 5 """
-    cur_dir = os.getcwd()
-    fonta_file = cur_dir + '/fonta.json'
+    """ Get icon name for fontawesome 5. This function is for compatibility """
     fonta = {}
-
-    if (not os.path.exists(fonta_file) and not
-            os.path.isfile(fonta_file)):
-
-        req = requests.get("https://origin.fontawesome.com/cheatsheet")
-
-        m = re.findall('window.__inline_data__ = (.*)', req.text)
-        json_m = json.loads(m[0])
-        json_data = json_m[0]['data']
-
-        with open(fonta_file, 'w') as f:
-            # json_data = json.load(f)
-            json.dump(json_data, f)
-            # f.write(json_data)
-
-    with open(fonta_file, 'r') as f:
-        json_data = json.load(f)
-
-    for icons in json_data:
-        free = icons['attributes']['membership']['free']
-        if (len(free)):
-            letter_free = free[0][0]
-            fonta[icons['id']] = "fa" + letter_free + " fa-" + icons['id']
-
     return fonta
 
 
 def search_icon_5(name, font_list):
-    if (name.lower() in font_list):
-        return font_list[name.lower()]
+    if (name.lower() in fa.icons.keys()):
+        return 'fas fa-' + name.lower()
     else:
         return None
 
